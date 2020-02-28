ef905caa3413306370b2c6ddd178b89fee5964f0
feline <zxf_tech@outlook.com>
2020-02-28 20:38:55 +0800

    更改了查询数据库的操作流程。

--- server/server.js
+++ server/server.js
@@ -58,23 +58,29 @@ io.on('connection', function(socket) {
     // 注册
     socket.on('register',function(userStatus) {
         var user = new User(userStatus);
-        findDocuments({'username':user.username},'testCollection',function(result){
-            if (result.length) {
-                socket.emit('regFailed','该用户名已存在.');
-            }
-            else {
-                insertDocument([user],'testCollection',function(result) {
-                    console.log(result.result.ok);
-                    if (result.result.ok==1) {
-                        console.log(user.username);
-                        socket.emit('logOrRegSuccessfully',user);
-                    }
-                    else {
-                        socket.emit('regFailed','出现问题，请稍后再试。');
-                    }
-                });
-                console.log(user);
-            }
+        findDocuments({'username':user.username},'testCollection',function(cursor){
+            cursor.toArray(function(err,result) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                if (result.length) {
+                    socket.emit('regFailed','该用户名已存在.');
+                }
+                else {
+                    insertDocument([user],'testCollection',function(result) {
+                        console.log(result.result.ok);
+                        if (result.result.ok==1) {
+                            console.log(user.username);
+                            socket.emit('logOrRegSuccessfully',user);
+                        }
+                        else {
+                            socket.emit('regFailed','出现问题，请稍后再试。');
+                        }
+                    });
+                    console.log(user);
+                }
+            })
         })
     });
 
@@ -82,18 +88,24 @@ io.on('connection', function(socket) {
     socket.on('login',function(userStatus) {
         console.log('someone try to login');
         var loginUser = new User(userStatus);
-        var user; findDocuments({'username':loginUser.username},'testCollection',function(result) {
-            if (result.length) {
-                if (result[0].password === loginUser.password) {
-                    socket.emit('logOrRegSuccessfully',result[0]);
+        var user; findDocuments({'username':loginUser.username},'testCollection',function(cursor) {
+            cursor.toArray(function(err,result){
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                if (result.length) {
+                    if (result[0].password === loginUser.password) {
+                        socket.emit('logOrRegSuccessfully',result[0]);
+                    }
+                    else {
+                        socket.emit('logFailed','密码不正确！');
+                    }
                 }
                 else {
-                    socket.emit('logFailed','密码不正确！');
+                    socket.emit('logFailed','用户名不存在！');
                 }
-            }
-            else {
-                socket.emit('logFailed','用户名不存在！');
-            }
+            })
         });
 
     })
@@ -114,10 +126,11 @@ function insertDocument(insertDocument,collectionName,callback) {
 // 查找
 function findDocuments(filter,collectionName,callback) {
     var collection = db.collection(collectionName);
-    collection.find(filter).toArray(function(err,docs){
-        assert.equal(err,null);
-        callback(docs);
-    });
+    callback(collection.find(filter));
+    // collection.find(filter).toArray(function(err,docs){
+    //     assert.equal(err,null);
+    //     callback(docs);
+    // });
 }
 
 // 更新
